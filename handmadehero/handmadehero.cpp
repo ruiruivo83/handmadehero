9d68f02e57522d54020c4ad1d7139731d01b13f0
Rui Ruivo <ruivo.rui@gmail.com>
2020-10-10 03:35:33 +0200

    Update
    Working OK, Window Resize.

--- handmadehero/handmadehero.cpp
+++ handmadehero/handmadehero.cpp
@@ -21,13 +21,58 @@
 
 // TODO(Rui): This is a global for now.
 global_variable bool Running;
+global_variable BITMAPINFO BitmapInfo;
+global_variable void* BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
 
-internal void ResizeDIBSection(int Width, int Height)
+internal void
+Win32ResizeDIBSection(int Width, int Height)
 {
+
+    // TODO(Rui): Bulletproof this.
+    // Maybe dont free first, free after then free first if that fails
+
+    // TODO(Rui): Free our DibSection
+
+    if (BitmapHandle) {
+        DeleteObject(BitmapHandle);
+    }
+
+    if (!BitmapDeviceContext)
+    {
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    BitmapDeviceContext = CreateCompatibleDC(0);
+    BitmapHandle = CreateDIBSection(
+        BitmapDeviceContext, &BitmapInfo,
+        DIB_RGB_COLORS,
+        &BitmapMemory,
+        0, 0);
 
 }
 
-LRESULT CALLBACK MainWindowCallBack(
+internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+    StretchDIBits(
+        DeviceContext,
+        X, Y, Width, Height,
+        X, Y, Width, Height,
+        BitmapMemory,
+        &BitmapInfo,
+        DIB_RGB_COLORS, SRCCOPY
+    );
+}
+
+LRESULT CALLBACK Win32MainWindowCallBack(
     HWND Window,
     UINT Message,
     WPARAM WParam,
@@ -47,7 +92,7 @@ LRESULT CALLBACK MainWindowCallBack(
         GetClientRect(Window, &ClientRect);
         int Width = ClientRect.right - ClientRect.left;
         int Height = ClientRect.bottom - ClientRect.top;
-        ResizeDIBSection(Width, Height);        
+        Win32ResizeDIBSection(Width, Height);
     } break;
 
     case WM_CLOSE:
@@ -65,7 +110,6 @@ LRESULT CALLBACK MainWindowCallBack(
 
     case WM_DESTROY:
     {
-
         // TODO(Rui): Handle this as error
         Running = false;
         OutputDebugStringA("WM_DESTROY\n");
@@ -79,16 +123,8 @@ LRESULT CALLBACK MainWindowCallBack(
         int Y = Paint.rcPaint.top;
         int Width = Paint.rcPaint.right - Paint.rcPaint.left;
         int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-        local_persist DWORD Operation = WHITENESS;
-        PatBlt(DeviceContext, X, Y, Width, Height, Operation);
-        if (Operation == WHITENESS) {
-            Operation = BLACKNESS;
-        }
-        else {
-            Operation = WHITENESS;
-        }
+        Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
         EndPaint(Window, &Paint);
-
     } break;
 
     default:
@@ -121,7 +157,7 @@ int main(
 
     // TODO - 
     WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-    WindowClass.lpfnWndProc = MainWindowCallBack;
+    WindowClass.lpfnWndProc = Win32MainWindowCallBack;
     WindowClass.hInstance = Instance;
     // WindowClass.hIcon;    
     WindowClass.lpszClassName = L"HandMadeHeroWindowClass";
